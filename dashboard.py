fa456277d3d28c8d64c841abf8b5a3c0bbaf2c0b
Nick Lyu <835021443@qq.com>
2020-06-25 15:23:34 -0400

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -30,7 +30,6 @@ import pandas as pd
 import csv
 
 
-# In[ ]:
 
 
 
@@ -66,7 +65,7 @@ app.layout = html.Div([
             {'label': 'SPY', 'value': 'SPY'},
             {'label': 'Coke', 'value': 'COKE'}
         ],
-        value='SPY',
+        value='MSFT',
     )],
         style = {'width': '100%', 'display': 'flex', 'align-items': 'center', 'justify-content': 'center'}
 
@@ -80,32 +79,37 @@ app.layout = html.Div([
                     id='date-range',
                     min_date_allowed=datetime(2015,1,1),
                     max_date_allowed=datetime.today(),
-                    start_date=datetime(2015,1,1),
+                    start_date=datetime(2018,1,1),
                     end_date=datetime.today()
                 )
             ],
              style = {'width': '100%', 'display': 'flex', 'align-items': 'center', 'justify-content': 'center'}
             ),
     html.Div([
-    dbc.Button(
-        html.H4(
-            'Get the Result',
-            style={'textAlign':'center','color':colors['text']}
-                ),
-               id='button',
-               color='dark',
-               n_clicks=0,
-               className='mr-1')
-    ],
-        style = {'width': '100%', 'display': 'flex', 'align-items': 'center', 'justify-content': 'center'}
-    ),
+        dbc.Button(
+            html.H4(
+                'Get the Result',
+                style={'textAlign':'center','color':colors['text']}
+                    ),
+                   id='button',
+                   color='dark',
+                   n_clicks=0,
+                   className='mr-1')
+        ],
+            style = {'width': '100%', 'display': 'flex', 'align-items': 'center', 'justify-content': 'center'}
+        ),
+    html.Div([dcc.Graph(id='candle')]),
+    html.Div([dcc.Graph(id='totalreturn')]),
+    html.Div([dcc.Graph(id='corheat')])
+
     
-    dcc.Graph(id='candle')
-    #dcc.Graph(id='radardailyreturn')
 ], style={'backgroundColor': colors['background'],'width': '500'}
 )
 
-@app.callback(Output('candle', 'figure'), 
+@app.callback([Output('candle', 'figure'),
+               Output('totalreturn', 'figure'),
+               Output('corheat', 'figure')
+              ], 
               [Input('button', 'n_clicks')],
               [State('my-tickers', 'value'),
                State('date-range', 'start_date'),
@@ -113,12 +117,18 @@ app.layout = html.Div([
 def update_graph(n_clicks,selected_dropdown_value,start_date,end_date):
     start = datetime.strptime(start_date[:10], '%Y-%m-%d')
     end = datetime.strptime(end_date[:10], '%Y-%m-%d')
+    dfcomb = pd.DataFrame(yf.download([selected_dropdown_value,'SPY'],start,end)['Adj Close']).reset_index()
     df = pd.DataFrame(yf.download(selected_dropdown_value,start,end)).reset_index()
+    df1 = pd.DataFrame(yf.download(selected_dropdown_value,start,end))['Adj Close'].reset_index()
+    dfspy = pd.DataFrame(yf.download('SPY',start,end)['Adj Close']).reset_index()
+
+
+    temp1 = (df1['Adj Close'].iloc[-1]/df1['Adj Close'].iloc[0]) * 100 - 100
+    temp2 = (dfspy['Adj Close'].iloc[-1]/dfspy['Adj Close'].iloc[0]) * 100 - 100
     df['MA30'] = df.Close.rolling(30).mean()
     df['MA100'] = df.Close.rolling(100).mean()
     
-    return {'data' : [ dict(
-        type = 'candlestick',
+    cd = go.Figure(data = [go.Candlestick(
         yaxis='y2',
         x = df.Date,
         open = df.Open,
@@ -126,71 +136,70 @@ def update_graph(n_clicks,selected_dropdown_value,start_date,end_date):
         low = df.Low,
         close = df.Close,
         name = selected_dropdown_value + ' Candlestick'
-        ), dict(
-        type = 'scatter',
+        ), go.Scatter(
         yaxis='y2',
         x = df.Date,
         y = df.MA30,
-        mode = 'lines',
+#         mode = 'lines',
         name = 'MA30',
         line = {'color': 'rgb(219, 64, 82)','width': 1}
-        ), dict(
-        type = 'scatter',
+        ), go.Scatter(
         yaxis='y2',
         x = df.Date,
         y = df.MA100,
-        mode = 'lines',
+#         mode = 'lines',
         name = 'MA100',
         line = {'color': 'green','width': 1}
-        ), dict(
+        ), go.Bar(
         x=df.Date, 
-        y=df.Volume,                         
-        type='bar', 
+        y=df.Volume,                          
         yaxis='y', 
         name='Volume',
-        mode = "markers",
+#         mode = "markers",
         marker = {'color': colors['text']}
         )],
             
-        'layout': {'margin': {'l': 40, 'r': 0, 't': 20, 'b': 30},
-                   'paper_bgcolor':'rgba(0,0,0,0)',
-                   'plot_bgcolor':'rgba(0,0,0,0)',
-                    'yaxis':{ 'domain': [0, 0.2] },
-                    'yaxis2':{ 'domain' : [0.2, 0.8] }}
-           }
+        layout= {'paper_bgcolor':'rgba(0,0,0,0)',
+                 'plot_bgcolor':'rgba(0,0,0,0)',
+                 'yaxis':{ 'domain': [0, 0.2] },
+                 'yaxis2':{ 'domain' : [0.2, 0.8] },
+                 'title':"Candlestick With MA And Volume",
+                 'font':{'size':18, 'color':colors['text']}
+                }
+              )
+    barr = go.Figure(data = [go.Bar(
+        x = [selected_dropdown_value, 'SPY'],
+        y = [temp1,temp2],
+        
+        )],
+        layout = {'yaxis':{'title':"Return Percentage"},
+                  'paper_bgcolor':'rgba(0,0,0,0)',
+                  'plot_bgcolor':'rgba(0,0,0,0)',
+                  'title':"Return: STOCK VS SPY",
+                  'font':{'size':18, 'color':colors['text']}
+                 }
+        )
+    corheat = go.Figure(data = [go.Heatmap(
+        x = [selected_dropdown_value, 'SPY'],
+        y = [selected_dropdown_value, 'SPY'],
+        z=dfcomb.corr(),
+        colorscale='Jet'
+        
+        )],
+        layout = {'paper_bgcolor':'rgba(0,0,0,0)',
+                  'plot_bgcolor':'rgba(0,0,0,0)',
+                  'title':"Correlation Between STOCK AND SPY",
+                  'font':{'size':18, 'color':colors['text']}
+                 }
+        )
+    return cd,barr, corheat
 
-# @app.callback(Output('radardailyreturn', 'figure'), 
-#               [Input('button', 'n_clicks')],
-#               [State('my-tickers', 'value'),
-#                State('date-range', 'start_date'),
-#                State('date-range', 'end_date')])
 
-# def update_graph(n_clicks,selected_dropdown_value,start_date,end_date):
-#     start = datetime.strptime(start_date[:10], '%Y-%m-%d')
-#     end = datetime.strptime(end_date[:10], '%Y-%m-%d')
-#     df = pd.DataFrame(yf.download(selected_dropdown_value,start,end)).reset_index()
-#     df[t + '_MA_30'] = df[t].rolling(30).mean()
-#     df[t + '_MA_100'] = df[t].rolling(100).mean()
-    
-#     return {'data' : [ dict(
-#     type = 'candlestick',
-#     x = df.Date,
-#     open = df.Open,
-#     high = df.High,
-#     low = df.Low,
-#     close = df.Close
-#     )],
-#     'layout': {'margin': {'l': 40, 'r': 0, 't': 20, 'b': 30},
-#                'paper_bgcolor':'rgba(0,0,0,0)',
-#                'plot_bgcolor':'rgba(0,0,0,0)'}}
 
 
 
 if __name__ == '__main__':
     app.run_server()
-
-
-# In[ ]:
 
 
 
